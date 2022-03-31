5724ad9778ccb830655bd9dfb78b6903bc07000f
RomaDevs9700 <ramazonzulfiqorov97@gmail.com>
2022-03-31 22:10:22 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', function() {
+    window.addEventListener('scroll', function() {
+        const header = this.document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 0)
+    })
+})
